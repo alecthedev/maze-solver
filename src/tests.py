30756606b19fe1a0dfc05175e6d36d48ad310000
alecthedev <alecthedeveloper@gmail.com>
2024-07-12 12:24:31 -0400

    add start and end cell tests

--- src/tests.py
+++ src/tests.py
@@ -1,20 +1,28 @@
 import unittest
 
 from graphics import Point, Window
+from hex import Hex
 from maze import Maze
 
 window = Window(0, 0)
+
+m1 = Maze(Point(0, 0), 5, 4, window)
+m2 = Maze(Point(0, 0), 5, 0, window)
 
 
 class Tests(unittest.TestCase):
     def test_spiral_size(self):
         # maze with 4 rings should have 61 hex cells
-        m1 = Maze(Point(0, 0), 5, 4, window)
         self.assertEqual(len(m1._hexes), 61)
 
         # maze with 0 rings should have only 1 cell (the center)
-        m2 = Maze(Point(0, 0), 5, 0, window)
         self.assertEqual(len(m2._hexes), 1)
+
+    def test_start_cell_wall(self):
+        self.assertFalse(m1._hexes[m1._start_pos].get_wall(3))
+
+    def test_end_cell_wall(self):
+        self.assertFalse(m1._hexes[m1._end_pos].get_wall(0))
 
 
 if __name__ == "__main__":
